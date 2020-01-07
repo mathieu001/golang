23193dcf8122142bd155e9d57e8d2e2d75b61e13
mathieu001 <scj02@163.com>
2020-01-07 15:04:24 +0800

    add quiz first version

---
+++ quiz/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "quiz.csv", "csv file")
+	timeLimit := flag.Int("time", 30, "the time limit for the quiz in second")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("failed to open %s", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("failed to parse the provider csv file")
+	}
+
+	problems := parseLines(lines)
+	fmt.Println(problems)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("question #%d, %s\n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
